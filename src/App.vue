b8afae84c39fc1fb078e479d919bb00c37e06103
aalsharifi <94931589+aalsharifi@users.noreply.github.com>
2022-02-13 22:33:56 -0500

    added footer

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,16 @@
   
   <Navbar />
 
-   <router-view v-if="categories && products"
-   :baseURL="baseURL" 
-   :categories="categories" 
-   :products="products"
-   @fetchData="fetchData"
-   >
-   </router-view>
+  <router-view v-if="categories && products" style="min-height: 60vh;"
+  :baseURL="baseURL" 
+  :categories="categories" 
+  :products="products"
+  @fetchData="fetchData"
+  >
+  </router-view>
    
+  <Footer />
+
   </div>
 
 </template>
@@ -18,8 +20,10 @@
 <script>
 import Navbar from "./components/Navbar.vue";
 import axios from "axios";
+import Footer from './components/Footer.vue';
+
 export default {
-  components: { Navbar },
+  components: { Navbar, Footer },
   data() {
     return{
       baseURL: "https://heroku-ecommerce-backend.herokuapp.com",
---
+++ src/components/Footer.vue
@@ -0,0 +1,64 @@
+<template>
+   <footer>
+       <div class="container pt-5">
+           <div class="row">
+               <div class="col-md-3 col-6">
+                   <ul style="list-style-type: none">
+                       <li class="text-light font-weight-bold pb-2">Get to know us</li>
+                       <li><a class="footer-link font-weight-light" href="#">About Us</a></li>
+                       <li><a class="footer-link font-weight-light" href="#">Sponsorships</a></li>
+                       <li><a class="footer-link font-weight-light" href="#">Download Mobile App</a></li>
+                   </ul>
+               </div>
+               <div class="col-md-3 col-6">
+                   <ul style="list-style-type: none">
+                       <li class="text-light font-weight-bold pb-2">Social Media</li>
+                       <li><a class="footer-link font-weight-light" href="#">Instagram</a></li>
+                       <li><a class="footer-link font-weight-light" href="#">Facebook</a></li>
+                       <li><a class="footer-link font-weight-light" href="#">Twitter</a></li>
+                   </ul>
+               </div>
+               <div class="col-md-3 col-6">
+                   <ul style="list-style-type: none">
+                       <li class="text-light font-weight-bold pb-2">Memberships</li>
+                       <li><a class="footer-link font-weight-light" href="#">Basic</a></li>
+                       <li><a class="footer-link font-weight-light" href="#">Silver</a></li>
+                       <li><a class="footer-link font-weight-light" href="#">Gold</a></li>
+                   </ul>
+               </div>
+               <div class="col-md-3 col-6">
+                   <ul style="list-style-type: none">
+                       <li class="text-light font-weight-bold pb-2">Get in Contact</li>
+                       <li><a class="footer-link font-weight-light" href="#">Return Center</a></li>
+                       <li><a class="footer-link font-weight-light" href="#">Customer service</a></li>
+                       <li><a class="footer-link font-weight-light" href="#">Help</a></li>
+                   </ul>
+               </div>
+           </div>
+           
+       </div>
+   </footer>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    footer{
+        margin-top: 100px;
+        background-color: #000;
+        font-size: 16px;
+    }
+    a{
+        text-decoration: none;
+    }
+    .footer-link{
+        color: #fff;
+    }
+    li{
+        padding-bottom: 10px;
+    }
+</style>
