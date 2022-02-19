d37db4b0da688b3047d1f7e2400ea00a0afe467c
aalsharifi <94931589+aalsharifi@users.noreply.github.com>
2022-02-18 23:07:40 -0500

    created signup.vue

---
+++ src/views/Signup.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center pt-3">
+                <!-- display logo -->
+            </div>
+        </div>
+
+        <!-- header -->
+
+
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    setup() {
+        
+    },
+}
+</script>
